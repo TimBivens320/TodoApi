3e28dd0b9ed11d8ccf85f6475492431080f4d108
TimBivens320 <tbivens320@gmail.com>
2021-07-22 10:24:55 -0400

    Add files via upload

---
+++ 20210623173049_add_dummy_descriptor.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace TodoApi.Migrations
+{
+    public partial class add_dummy_descriptor : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "TodoItems",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "DummyDescriptors",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Description = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DummyDescriptors", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DummyDescriptors");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "TodoItems");
+        }
+    }
+}
---
+++ DummyDescriptor.cs
@@ -0,0 +1,8 @@
+namespace TodoApi.Models {     
+    public class DummyDescriptor   
+    {        
+    
+        public long Id { get; set; }        
+        public string Description { get; set; }
+    }
+}
